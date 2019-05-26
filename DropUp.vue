d6ae57f3099aab73515e8510c284f78ad4479d8e
nachmanrosen <nachmanrosen@gmail.com>
2019-05-26 18:06:30 -0400

    dropUp 1.2

--- DropUp.vue
+++ DropUp.vue
@@ -1,79 +1,83 @@
 <template>
     <div class="dropup">
-        <button @click="clear()" class="dropbtn">
+        <div class="dropbtn">
             {{ text }}
-        </button>
+        </div>
         <div class="dropup-content">
-            <div class="options" @click="clear">{{ buttonText }}</div>
-            <div class="options" @click="select(option)" v-for="option in options">{{ option.name }}</div>
+            <div class="options" @click="clear">Clear</div>
+            <div class="options" @click="select(option)" v-for="option in options">{{ option.name }}
+                <span v-if="option.price">{{option.price}}</span></div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'DropUp',
-    props: ['options', 'buttonText'],
-    data() {
-        return {
-            text: this.buttonText,
-        };
-    },
-    methods: {
-        select(option) {
-            this.text = option.name;
-            this.$emit('selected', option);
+    export default {
+        name: 'DropUp',
+        props: ['options', 'multiSelect'],
+        data() {
+            return {
+                text: '',
+                selected: '',
+            };
         },
-        clear() {
-            this.text = this.buttonText;
-            this.$emit('clear');
+        methods: {
+            select(option) {
+                this.text = this.selected.map(o =>o.name);
+                this.multiSelect ? this.selected = item : this.selected.push(item);
+            },
+            clear() {
+                this.text = '';
+                this.selected = {};
+            },
         },
-    },
-};
+    };
 </script>
 
 <style scoped>
-.dropbtn {
-    color: white;
-    padding: 1px;
-    font-size: 11px;
-    border: none;
-    margin-right: 0.5rem;
-}
-.img {
-    margin-left: 2rem;
-}
+    .dropbtn {
+        color: white;
+        padding: 1px;
+        font-size: 11px;
+        border: none;
+        margin-right: 0.5rem;
+    }
 
-.dropup {
-    width: 5rem;
-    height: 5rem;
-    position: relative;
-    display: inline-block;
-}
+    .img {
+        margin-left: 2rem;
+    }
 
-.dropup-content {
-    display: none;
-    position: absolute;
-    background-color: white;
-    min-width: 160px;
-    bottom: 5rem;
-    z-index: 1;
-    border-radius: 3px;
-    box-shadow: 0 2px 9px 0 #00000080;
-}
-.options {
-    padding: 1rem;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
+    .dropup {
+        width: 5rem;
+        height: 5rem;
+        position: relative;
+        display: inline-block;
+    }
 
-.options:hover {
-    background-color: #e6eaf2;
-}
+    .dropup-content {
+        display: none;
+        position: absolute;
+        background-color: white;
+        min-width: 160px;
+        bottom: 5rem;
+        z-index: 1;
+        border-radius: 3px;
+        box-shadow: 0 2px 9px 0 #00000080;
+    }
 
-.dropup:hover .dropup-content {
-    display: block;
-}
+    .options {
+        padding: 1rem;
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .options:hover {
+        background-color: #e6eaf2;
+    }
+
+    .dropup:hover .dropup-content {
+        display: block;
+    }
 </style>
---
+++ DropUpContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <drop-up
+                :multiselect="multiSelect"
+                :options="myOptions"
+        ></drop-up>
+    </div>
+    </template>
+    <script>
+        import DropUp from "./DropUp";
+        export default {
+            name: "DropUpContainer",
+            data(){
+                return{
+                    myOptions:[{name:'apples', price:'$2.00'}],
+                    multiSelect="true"
+                }
+            },
+            components: {DropUp}
+        }
+    </script>
